3f1826f0570a91f0e7d6c28f7519708cc1d14886
blinry <mail@blinry.org>
2024-01-16 17:41:24 +0100

    Draw test text on the e-ink screen

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,27 @@
 use core::time::Duration;
 
 use esp32_hal::{
-    clock::ClockControl, entry, gpio::IO, i2c::I2C, peripherals::Peripherals, prelude::*,
-    rtc_cntl::sleep::TimerWakeupSource, Delay, Rtc,
+    clock::ClockControl,
+    entry,
+    gpio::{IO, NO_PIN},
+    i2c::I2C,
+    peripherals::Peripherals,
+    prelude::*,
+    rtc_cntl::sleep::TimerWakeupSource,
+    spi::{master::Spi, SpiMode},
+    Delay, Rtc,
 };
 use esp_backtrace as _;
 use esp_println::println;
 
-/*
 use embedded_graphics::{
+    mono_font::{ascii::FONT_10X20, MonoTextStyle},
     pixelcolor::BinaryColor::On as Black,
     prelude::*,
     primitives::{Line, PrimitiveStyle},
+    text::Text,
 };
 use epd_waveshare::{epd2in9_v2::*, prelude::*};
-*/
 
 const SUNRISE_ADDR: u8 = 0x68;
 
@@ -29,6 +36,7 @@ fn main() -> ! {
     let mut delay = Delay::new(&clocks);
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
+    /*
     let sda = io.pins.gpio22;
     let scl = io.pins.gpio20;
 
@@ -76,58 +84,68 @@ fn main() -> ! {
     i2c.write_read(SUNRISE_ADDR, &[0x06], &mut buf).unwrap();
     let co2 = u16::from_be_bytes(buf);
     println!("CO2: {}", co2);
-
-    // Go to sleep.
-    let timer = TimerWakeupSource::new(Duration::from_secs(10));
-    println!("sleeping!");
-    delay.delay_ms(100u32);
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    rtc.sleep_deep(&[&timer], &mut delay);
+    */
 
     /*
 
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
 
     println!("up and runnning!");
     let reason = get_reset_reason(Cpu::ProCpu).unwrap_or(SocResetReason::ChipPowerOn);
     println!("reset reason: {:?}", reason);
     let wake_reason = get_wakeup_cause();
     println!("wake reason: {:?}", wake_reason);
+    */
 
     // Pins for ESP32 Feather v2 + E-Ink Feather Wing
+
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+
     let sck = io.pins.gpio5;
     let mosi = io.pins.gpio19;
     let miso = io.pins.gpio21;
-    let cs = io.pins.gpio16; // ?
-    let mut spi = Spi::new(peripherals.SPI3, 1000u32.kHz(), SpiMode::Mode0, &clocks).with_pins(
+
+    let mut spi = Spi::new(peripherals.SPI3, 4000u32.kHz(), SpiMode::Mode0, &clocks).with_pins(
         Some(sck),
         Some(mosi),
         Some(miso),
-        Some(cs),
+        NO_PIN,
     );
 
-    let cs_pin = io.pins.gpio15.into_push_pull_output();
-    let busy_in = io.pins.gpio27.into_pull_down_input(); // ?
-    let dc = io.pins.gpio33.into_push_pull_output();
-    let rst = io.pins.gpio12.into_push_pull_output(); // ?
+    let mut cs = io.pins.gpio15.into_push_pull_output(); // chip select
+    cs.set_high().unwrap();
+    let busy_in = io.pins.gpio26.into_pull_down_input(); // ?
+    let dc = io.pins.gpio33.into_push_pull_output(); // data/command
+    let rst = io.pins.gpio13.into_push_pull_output(); // ?
     let mut delay = Delay::new(&clocks);
 
-
-    let mut epd = Epd2in9::new(&mut spi, cs_pin, busy_in, dc, rst, &mut delay).unwrap();
+    let mut epd = Epd2in9::new(&mut spi, cs, busy_in, dc, rst, &mut delay).unwrap();
 
     // Use display graphics from embedded-graphics
     let mut display = Display2in9::default();
 
-    // Use embedded graphics for drawing a line
+    //epd.wake_up(&mut spi, &mut delay).unwrap();
+    //epd.clear_frame(&mut spi, &mut delay).unwrap();
+    //epd.display_frame(&mut spi, &mut delay).unwrap();
 
-    let _ = Line::new(Point::new(0, 0), Point::new(20, 20))
+    //println!("cleared!");
+    //delay.delay_ms(2000u32);
+    //println!("waited!");
+
+    // Use embedded graphics for drawing a line
+    let _ = Line::new(Point::new(0, 0), Point::new(20, 10))
         .into_styled(PrimitiveStyle::with_stroke(Black, 1))
         .draw(&mut display);
 
-    // Display updated frame
+    let text = "ha this does work";
+    let style = MonoTextStyle::new(&FONT_10X20, Black);
+    let _ = Text::new(text, Point::new(30, 30), style).draw(&mut display);
+
     epd.update_frame(&mut spi, display.buffer(), &mut delay)
         .unwrap();
     epd.display_frame(&mut spi, &mut delay).unwrap();
+    println!("drawn!");
+    delay.delay_ms(1000u32);
+    println!("waited!");
 
     // Set the EPD to sleep
     epd.sleep(&mut spi, &mut delay).unwrap();
@@ -137,7 +155,6 @@ fn main() -> ! {
     println!("sleeping!");
     delay.delay_ms(100u32);
     rtc.sleep_deep(&[&timer], &mut delay);
-    */
 }
 
 fn get_byte<T>(i2c: &mut I2C<T>, reg: u8) -> u8
