7d4589e882db0e74dffb684babf376170bd0e184
blinry <mail@blinry.org>
2024-10-19 11:04:11 +0200

    Adapt to new crates

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,32 @@ mod display;
 mod history;
 mod sunrise;
 
-use core::time::Duration;
+use core::{
+    ptr::{addr_of, addr_of_mut},
+    time::Duration,
+};
 use display::Display;
+use embedded_hal::delay::DelayNs;
 use history::History;
 use sunrise::{CalibrationData, SunriseSensor};
 
 use embedded_hal_bus::spi::ExclusiveDevice;
-use esp32_hal::{
-    adc::{AdcConfig, Attenuation, ADC},
-    analog::{ADC1, ADC2},
-    clock::ClockControl,
+use esp_backtrace as _;
+use esp_hal::{
+    analog::adc::{Adc, AdcConfig, Attenuation},
+    delay::Delay,
     entry,
-    gpio::{IO, NO_PIN},
-    i2c::I2C,
-    peripherals::Peripherals,
+    gpio::{Input, Io, Level, NoPin, Output, Pull},
+    i2c::I2c,
     prelude::*,
     rtc_cntl::{
         get_reset_reason, get_wakeup_cause,
         sleep::{RtcSleepConfig, TimerWakeupSource},
-        SocResetReason,
+        Rtc, SocResetReason,
     },
     spi::{master::Spi, SpiMode},
-    Cpu, Delay, Rtc,
+    Cpu,
 };
-use esp_backtrace as _;
 use esp_println::println;
 
 #[ram(rtc_fast)]
@@ -39,11 +41,9 @@ static mut CALIBRATION_DATA: CalibrationData = CalibrationData::new();
 
 #[entry]
 fn main() -> ! {
-    let peripherals = Peripherals::take();
-    let system = peripherals.SYSTEM.split();
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
-    let mut delay = Delay::new(&clocks);
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    let mut delay = Delay::new();
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
     let mut rtc = Rtc::new(peripherals.LPWR);
 
     let reason = get_reset_reason(Cpu::ProCpu).unwrap_or(SocResetReason::ChipPowerOn);
@@ -56,11 +56,11 @@ fn main() -> ! {
     //let mut adc1 = ADC::adc(analog.adc1, adc1_config).unwrap();
 
     if true {
-        let co2_enable = io.pins.gpio25.into_push_pull_output();
+        let co2_enable = Output::new(io.pins.gpio25, Level::High);
 
         let sda = io.pins.gpio22;
         let scl = io.pins.gpio20;
-        let i2c = I2C::new(peripherals.I2C0, sda, scl, 100u32.kHz(), &clocks);
+        let i2c = I2c::new(peripherals.I2C0, sda, scl, 100u32.kHz());
 
         let number_of_samples = 2;
         let mut co2_sensor = SunriseSensor::new(i2c, co2_enable, &mut delay);
@@ -74,18 +74,17 @@ fn main() -> ! {
                 .expect("Could not start CO2 measurement");
         }
 
-        let mut delay = Delay::new(&clocks);
         let milliseconds_per_sample = 300;
         let timer = TimerWakeupSource::new(Duration::from_millis(
             (number_of_samples * milliseconds_per_sample) as u64,
         ));
-        rtc.sleep_light(&[&timer], &mut delay);
+        rtc.sleep_light(&[&timer]);
 
         unsafe {
             let co2 = co2_sensor.get_co2(&mut CALIBRATION_DATA).unwrap();
             println!("CO2: {} ppm", co2);
 
-            CALIBRATION_DATA.update_time_ms(rtc.get_time_ms());
+            CALIBRATION_DATA.update_time_ms(rtc.time_since_boot().ticks());
 
             HISTORY.add_measurement(co2);
             println!("{:?}", CALIBRATION_DATA);
@@ -99,10 +98,10 @@ fn main() -> ! {
     // read battery voltage from GPIO pin 15
     let mut adc1_config = AdcConfig::new();
     let mut battery_voltage_pin =
-        adc1_config.enable_pin(io.pins.gpio35.into_analog(), Attenuation::Attenuation11dB);
-    let analog = peripherals.SENS.split();
-    let mut adc1 = ADC::<ADC1>::adc(analog.adc1, adc1_config).unwrap();
-    let battery_voltage = nb::block!(adc1.read(&mut battery_voltage_pin)).unwrap() as f32;
+        adc1_config.enable_pin(io.pins.gpio35, Attenuation::Attenuation11dB);
+    //let analog = peripherals.SENS.split();
+    let mut adc1 = Adc::new(peripherals.ADC1, adc1_config);
+    let battery_voltage = nb::block!(adc1.read_oneshot(&mut battery_voltage_pin)).unwrap() as f32;
     println!("Measured value: {}", battery_voltage);
     // No idea why 1.14 is a good factor here, but it matches my measurements at full battery level.
     // battery voltage 4.22 V <-> 1610 measured value
@@ -116,20 +115,17 @@ fn main() -> ! {
         let mosi = io.pins.gpio19;
         let miso = io.pins.gpio21;
 
-        let mut spi = Spi::new(peripherals.SPI3, 16u32.MHz(), SpiMode::Mode0, &clocks).with_pins(
-            Some(sck),
-            Some(mosi),
-            Some(miso),
-            NO_PIN,
-        );
+        let mut spi = Spi::new(peripherals.SPI3, 16u32.MHz(), SpiMode::Mode0)
+            .with_pins(sck, mosi, miso, NoPin);
 
-        let cs = io.pins.gpio15.into_push_pull_output(); // chip select
-        let busy_in = io.pins.gpio39.into_pull_down_input();
-        let dc = io.pins.gpio33.into_push_pull_output(); // data/command
-        let rst = io.pins.gpio26.into_push_pull_output();
+        let cs = Output::new(io.pins.gpio15, Level::High); // chip select
+        let busy_in = Input::new(io.pins.gpio39, Pull::Down);
+        let dc = Output::new(io.pins.gpio33, Level::Low); // data/command
+        let rst = Output::new(io.pins.gpio26, Level::Low);
 
-        let exclusive_spi = ExclusiveDevice::new(&mut spi, cs, &mut delay);
-        let mut delay = Delay::new(&clocks);
+        let exclusive_spi = ExclusiveDevice::new(&mut spi, cs, &mut delay)
+            .expect("Failed to get exclusive SPI device");
+        let mut delay = Delay::new();
 
         let mut display = Display::new(exclusive_spi, busy_in, dc, rst, &mut delay);
 
@@ -142,17 +138,17 @@ fn main() -> ! {
 
     // Power off the neopixel and I2C bus, for low-power sleep.
     // See https://learn.adafruit.com/adafruit-esp32-feather-v2/power-management
-    let mut neopixel_and_i2c_power = io.pins.gpio2.into_push_pull_output();
-    neopixel_and_i2c_power.set_low().unwrap();
+    let _neopixel_and_i2c_power = Output::new(io.pins.gpio2, Level::Low);
+    //neopixel_and_i2c_power.set_low().unwrap();
 
     // Deep sleep.
-    let mut delay = Delay::new(&clocks);
+    let mut delay = Delay::new();
     let timer = TimerWakeupSource::new(Duration::from_secs(55));
     println!("sleeping!");
     delay.delay_ms(100u32);
 
     let mut cfg = RtcSleepConfig::deep();
     cfg.set_rtc_fastmem_pd_en(false);
-    rtc.sleep(&cfg, &[&timer], &mut delay);
+    rtc.sleep(&cfg, &[&timer]);
     panic!("We should never get here after the sleep() call.");
 }
