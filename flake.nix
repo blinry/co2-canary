1e1492316bc8ee0f57b41209c6564cc0f0fe4bdd
blinry <mail@blinry.org>
2024-10-19 15:10:52 +0200

    Fix Nix shell

--- flake.nix
+++ flake.nix
@@ -7,27 +7,25 @@
     };
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , esp32
-    ,
-    }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-      idf-rust = esp32.packages.x86_64-linux.esp32;
-    in
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = [
-          idf-rust
-          pkgs.cargo-espflash
-        ];
+  outputs = {
+    self,
+    nixpkgs,
+    esp32,
+  }: let
+    pkgs = import nixpkgs {system = "x86_64-linux";};
+    idf-rust = esp32.packages.x86_64-linux.esp32;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = [
+        idf-rust
+        pkgs.cargo-espflash
+      ];
 
-        shellHook = ''
-          export PATH="${idf-rust}/.rustup/toolchains/esp/bin:${idf-rust}/.rustup/toolchains/esp/xtensa-esp32-elf/esp-2021r2-patch5-8_4_0/xtensa-esp32-elf/bin:$PATH"
-          export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
-        '';
-      };
+      shellHook = ''
+        export PATH="${idf-rust}/.rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin:$PATH"
+        export PATH="${idf-rust}/.rustup/toolchains/esp/bin:$PATH"
+        export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+      '';
     };
+  };
 }
