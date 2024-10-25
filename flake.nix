4839ff7cffefc2ac77117c660cf916ca757ac06f
blinry <mail@blinry.org>
2024-10-25 11:50:28 +0200

    Try to adapt to ESP32-C6, not working yet

--- flake.nix
+++ flake.nix
@@ -1,31 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    esp32 = {
-      url = "github:knarkzel/esp32";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    esp32,
-  }: let
+  outputs = {nixpkgs, ...}: let
     pkgs = import nixpkgs {system = "x86_64-linux";};
-    idf-rust = esp32.packages.x86_64-linux.esp32;
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
       buildInputs = [
-        idf-rust
         pkgs.cargo-espflash
       ];
-
-      shellHook = ''
-        export PATH="${idf-rust}/.rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin:$PATH"
-        export PATH="${idf-rust}/.rustup/toolchains/esp/bin:$PATH"
-        export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
-      '';
     };
   };
 }
--- src/main.rs
+++ src/main.rs
@@ -54,10 +54,10 @@ fn main() -> ! {
     let mut temperature = 0.0;
 
     if true {
-        let co2_enable = Output::new(io.pins.gpio25, Level::High);
+        let co2_enable = Output::new(io.pins.gpio3, Level::High);
 
-        let sda = io.pins.gpio22;
-        let scl = io.pins.gpio20;
+        let sda = io.pins.gpio19;
+        let scl = io.pins.gpio18;
         let i2c = I2c::new(peripherals.I2C0, sda, scl, 100u32.kHz());
 
         let number_of_samples = 2;
@@ -99,33 +99,20 @@ fn main() -> ! {
         co2_sensor.turn_off();
     }
 
-    // read battery voltage from GPIO pin 15
-    let mut adc1_config = AdcConfig::new();
-    let mut battery_voltage_pin =
-        adc1_config.enable_pin(io.pins.gpio35, Attenuation::Attenuation11dB);
-    //let analog = peripherals.SENS.split();
-    let mut adc1 = Adc::new(peripherals.ADC1, adc1_config);
-    let battery_voltage = nb::block!(adc1.read_oneshot(&mut battery_voltage_pin)).unwrap() as f32;
-    println!("Measured value: {}", battery_voltage);
-    // No idea why 1.14 is a good factor here, but it matches my measurements at full battery level.
-    // battery voltage 4.22 V <-> 1610 measured value
-    // battery voltage 4.00 V <-> 1744 measured value
-    let battery_voltage = battery_voltage * 1.16 * 2.0 / 1000.0;
-
-    println!("Estimated battery voltage: {:.2} V\n", battery_voltage);
+    let battery_voltage = 0.0;
 
     if true {
-        let sck = io.pins.gpio5;
-        let mosi = io.pins.gpio19;
-        let miso = io.pins.gpio21;
+        let sck = io.pins.gpio21;
+        let mosi = io.pins.gpio22;
+        let miso = io.pins.gpio23;
 
-        let mut spi = Spi::new(peripherals.SPI3, 16u32.MHz(), SpiMode::Mode0)
+        let mut spi = Spi::new(peripherals.SPI2, 16u32.MHz(), SpiMode::Mode0)
             .with_pins(sck, mosi, miso, NoPin);
 
-        let cs = Output::new(io.pins.gpio15, Level::High); // chip select
-        let busy_in = Input::new(io.pins.gpio39, Pull::Down);
-        let dc = Output::new(io.pins.gpio33, Level::Low); // data/command
-        let rst = Output::new(io.pins.gpio26, Level::Low);
+        let cs = Output::new(io.pins.gpio7, Level::High); // chip select
+        let busy_in = Input::new(io.pins.gpio5, Pull::Down);
+        let dc = Output::new(io.pins.gpio8, Level::Low); // data/command
+        let rst = Output::new(io.pins.gpio1, Level::Low);
 
         let exclusive_spi = ExclusiveDevice::new(&mut spi, cs, &mut delay)
             .expect("Failed to get exclusive SPI device");
@@ -141,8 +128,8 @@ fn main() -> ! {
     }
 
     // Power off the neopixel and I2C bus, for low-power sleep.
-    // See https://learn.adafruit.com/adafruit-esp32-feather-v2/power-management
-    let _neopixel_and_i2c_power = Output::new(io.pins.gpio2, Level::Low);
+    // See https://learn.adafruit.com/adafruit-esp32-c6-feather/low-power-use
+    let _neopixel_and_i2c_power = Output::new(io.pins.gpio20, Level::Low);
     //neopixel_and_i2c_power.set_low().unwrap();
 
     // Deep sleep.
@@ -151,8 +138,9 @@ fn main() -> ! {
     println!("sleeping!");
     delay.delay_ms(100u32);
 
-    let mut cfg = RtcSleepConfig::deep();
-    cfg.set_rtc_fastmem_pd_en(false);
-    rtc.sleep(&cfg, &[&timer]);
-    panic!("We should never get here after the sleep() call.");
+    //let mut cfg = RtcSleepConfig::deep();
+    //cfg.set_rtc_fastmem_pd_en(false);
+    //rtc.sleep(&cfg, &[&timer]);
+    //panic!("We should never get here after the sleep() call.");
+    loop {}
 }
